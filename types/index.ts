729e8f11f1be1b3b7373c6f14cb0a7a1c35cfac8
uxelexx <i@kkornilov.ru>
2023-12-15 19:05:03 +0300

    types for actions

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,27 @@
-export type Board = {
-  rows: number;
-  columns: number;
-  grid: string[][];
-};
+export type Board = (Player | "")[][];
 
 export type Player = "X" | "O";
-
 export type Winner = Player | "Tie" | null;
-
 export type PlayerTime = Record<Player, number>;
+export type PlayerScore = Record<Player, number>;
+
+export type InitialState = {
+  board: Board;
+  start: boolean;
+  player: Player;
+  winner: Winner;
+  timer: PlayerTime;
+  score: PlayerScore;
+};
+
+export type Actions =
+  | { type: "SET_GAME"; payload: boolean }
+  | { type: "START_PAUSE" }
+  | { type: "ADD_PIECE"; payload: Board }
+  | { type: "SET_TIMER"; payload: Player }
+  | { type: "SET_WINNER"; payload: Winner }
+  | { type: "UPDATE_SCORE"; payload: Player }
+  | { type: "SET_PLAYER"; payload: Player }
+  | { type: "RESET_TIMER"; payload: Player }
+  | { type: "RESET_TURN_TIMER"; payload: Player }
+  | { type: "RESET_GAME" | "RESET_SCORE" };
