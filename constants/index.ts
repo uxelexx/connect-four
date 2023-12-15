55371de0e1b66b6acec9eae078765d6498bffb66
uxelexx <i@kkornilov.ru>
2023-12-15 19:05:11 +0300

    constants

---
+++ constants/index.ts
@@ -0,0 +1,16 @@
+export const ROWS = 6;
+export const COLUMNS = 7;
+export const ROUND_TIME = 5;
+
+export const ADD_PIECE = "ADD_PIECE";
+export const SET_PLAYER = "SET_PLAYER";
+export const SET_WINNER = "SET_WINNER";
+export const SET_TIMER = "SET_TIMER";
+export const UPDATE_SCORE = "UPDATE_SCORE";
+export const SET_GAME = "SET_GAME";
+export const START_PAUSE = "START_PAUSE";
+
+export const RESET_TIMER = "RESET_TIMER";
+export const RESET_SCORE = "RESET_SCORE";
+export const RESET_GAME = "RESET_GAME";
+export const RESET_TURN_TIMER = "RESET_TURN_TIMER";
