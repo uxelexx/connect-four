439aaa4389b19d9a371847a26180cc1e20cd54ae
uxelexx <i@kkornilov.ru>
2023-12-15 19:05:48 +0300

    remove unnecessary grid

--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -11,11 +11,11 @@ type BoardProps = {
 function Board({ board, addPiece, player }: BoardProps) {
   return (
     <div className={styles.board}>
-      {board.grid.map((row, rowIdx) => (
+      {board.map((row, rowIdx) => (
         <div key={rowIdx} className={styles.row}>
           {row.map((_, columnIdx) => {
             let active = "";
-            const activeCell = board.grid[rowIdx][columnIdx];
+            const activeCell = board[rowIdx][columnIdx];
 
             if (activeCell === "X") active = styles["X"];
             if (activeCell === "O") active = styles["O"];
